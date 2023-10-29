353860c81f3379575f003e68fd33981579c82aaa
WebApiAutomationTesting <149289336+WebApiAutomationTesting@users.noreply.github.com>
2023-10-29 16:55:06 +0530

    Add files via upload

---
+++ hybrid-automation/src/main/java/com/visible/steps/HomePageStep.java
@@ -0,0 +1,27 @@
+package com.visible.steps;
+
+import com.visible.drivers.Drivermanager;
+import com.visible.pages.HomePage;
+import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class HomePageStep {
+    HomePage homePage = new HomePage(Drivermanager.getDriver());
+
+    @Given("I navigate to landing page {string}")
+    public void iNavigateToLandingPage(String endponint) {
+        String baseUrl = "https://www.visible.com";
+        homePage.navigateTo(baseUrl+endponint);
+        RestAssured.baseURI=baseUrl;
+        RequestSpecification requestSpecification = RestAssured.given();
+        Response response = requestSpecification.request(Method.GET, endponint);
+        System.out.println(response.statusCode());
+        System.out.println(response.statusLine());
+
+        System.out.println(homePage.getPageTitle());
+
+    }
+}
